5570deaa09be9c75b562b3482b50cca77fe337ad
Ranranruo <tlsalstjr58@gmail.com>
2025-03-17 08:38:03 +0900

    a

--- BModule02/script/sub02.js
+++ BModule02/script/sub02.js
@@ -1,22 +1,26 @@
+// 컨트롤러 버튼들
 const $$btns = document.querySelectorAll("#ad button");
+// 비디오 요소
 const $ad = document.querySelector("video");
 
+// 이벤트들
 const adEvents = {
     play: () => $ad.play(),
     pause: () => $ad.pause(),
     stop: () => { $ad.pause(); $ad.currentTime = 0},
     back: () => $ad.currentTime = $ad.currentTime <= 10 ? 0 : $ad.currentTime - 10,
     go: () => $ad.currentTime = $ad.currentTime + 10 > $ad.duration ? $ad.duration : $ad.currentTime + 10,
-    slow: () => {},
-    fast: () => {},
-    clear: () => {},
-    toggle: () => {},
-    repeat: () => {},
-    auto: () => {}
+    slow: () => $ad.playbackRate > .11 ? $ad.playbackRate -= .1 : null,
+    fast: () => $ad.playbackRate += .1,
+    clear: () => $ad.playbackRate = 1,
+    toggle: () => $ad.toggleAttribute("controls"),
+    repeat: () => $ad.loop = !$ad.loop,
+    auto: () => $ad.autoplay = !$ad.autoplay
 }
 
+// 이벤트 적용
 $$btns.forEach($btn => $btn.addEventListener("click", (event) => {
     const evnetName = event.target.dataset.event;
-    console.log($ad.currentTime);
     adEvents[evnetName]();
+    console.log($ad.playbackRate);
 }));
